2ef18e63cd90d935b98fbc5f3eb721c5f44df6ad
ultraman <1394466835@qq.com>
2024-09-23 21:16:56 +0800

    feat: add footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,5 +1,27 @@
 function SiteFooter() {
-  return <footer className="">footer</footer>;
+  return (
+    <footer className="h-16 bg-background text-secondary-foreground print:hidden">
+      <div className="container mx-auto h-full flex flex-col md:flex-row items-center justify-center md:justify-between gap-1">
+        <span className="text-sm">
+          © 2024&nbsp;
+          <a
+            rel="author noopener"
+            className="hover:underline text-primary"
+            href="https://xmchxup.github.io"
+          >
+            Xmchx
+          </a>
+          &nbsp; All rights reserved.
+        </span>
+        <span className="text-sm">
+          Powered by&nbsp;
+          <a className="hover:underline text-primary" href="https://nextjs.org">
+            Next.js
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
 }
 
 export default SiteFooter;

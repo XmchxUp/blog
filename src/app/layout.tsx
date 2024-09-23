45f8d4f8a3307104713a3709054fa07199791bf9
ultraman <1394466835@qq.com>
2024-09-23 21:10:45 +0800

    feat add static tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
 
                   {/* Categories Card */}
                   <div className="rounded-xl border bg-card text-card-foreground shadow">
-                    <div className="flex flex-col space-y-1.5 p-6 pb-4">
+                    <div className="flex flex-col space-y-1.5 p-6 pb-2 pt-4">
                       <h3 className="text-md font-normal tracking-tight">
                         Categories
                       </h3>
@@ -119,6 +119,90 @@ export default function RootLayout({
                           Moments
                         </span>
                         <span className="text-sm">20</span>
+                      </Link>
+                    </div>
+                  </div>
+
+                  {/* Archives Card */}
+                  <div className="rounded-xl border bg-card text-card-foreground shadow">
+                    <div className="flex flex-col space-y-1.5 p-6 pb-2 pt-4">
+                      <h3 className="text-md font-normal tracking-tight">
+                        Archives
+                      </h3>
+                    </div>
+                    <div className="flex flex-col space-y-1 px-4 p-6 pt-0">
+                      <Link
+                        href="/category/life"
+                        className="items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-xs flex flex-row gap-2 w-full font-normal"
+                      >
+                        <span className="text-sm flex-grow truncate">2024</span>
+                        <span className="text-sm">2</span>
+                      </Link>
+                      <Link
+                        href="/category/life"
+                        className="items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-xs flex flex-row gap-2 w-full font-normal"
+                      >
+                        <span className="text-sm flex-grow truncate">2023</span>
+                        <span className="text-sm">20</span>
+                      </Link>
+                    </div>
+                  </div>
+
+                  {/* Tags Card */}
+                  <div className="rounded-xl border bg-card text-card-foreground shadow">
+                    <div className="flex flex-col space-y-1.5 p-6 pb-2 pt-4">
+                      <h3 className="text-md font-normal tracking-tight">
+                        Tags
+                      </h3>
+                    </div>
+                    <div className="p-6 flex flex-row flex-wrap items-center pt-2 px-4">
+                      <Link
+                        href="/category/life"
+                        className="inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-sm font-normal"
+                      >
+                        Algorithm
+                      </Link>
+                      <Link
+                        href="/category/life"
+                        className="inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-sm font-normal"
+                      >
+                        Theory
+                      </Link>
+                      <Link
+                        href="/category/life"
+                        className="inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-sm font-normal"
+                      >
+                        Cloud Native
+                      </Link>
+                      <Link
+                        href="/category/life"
+                        className="inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-sm font-normal"
+                      >
+                        Kata
+                      </Link>
+                      <Link
+                        href="/category/life"
+                        className="inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-sm font-normal"
+                      >
+                        Algorithm
+                      </Link>
+                      <Link
+                        href="/category/life"
+                        className="inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-sm font-normal"
+                      >
+                        Theory
+                      </Link>
+                      <Link
+                        href="/category/life"
+                        className="inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-sm font-normal"
+                      >
+                        Cloud Native
+                      </Link>
+                      <Link
+                        href="/category/life"
+                        className="inline-flex items-center justify-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-sm font-normal"
+                      >
+                        Kata
                       </Link>
                     </div>
                   </div>

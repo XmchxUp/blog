7078ac4f961d8870fb68481c46cfd6f6c1df8948
Tesla <1394466835@qq.com>
2024-09-25 11:06:44 +0800

    feat: add post detail

---
+++ src/app/post/[...slug]/page.tsx
@@ -0,0 +1,56 @@
+import { posts } from "#site/content";
+import { formatDate } from "@/lib/utils";
+import { notFound } from "next/navigation";
+import { MDXContent } from "@/components/mdx-components";
+import "@/app/mdx.css";
+
+interface PostPageProps {
+  params: {
+    slug: string[];
+  };
+}
+
+async function getPostFromParams(params: PostPageProps["params"]) {
+  const slug = params?.slug?.join("/");
+  const post = posts.find((post) => post.slugAsParams == slug);
+  return post;
+}
+
+async function PostPage({ params }: PostPageProps) {
+  const post = await getPostFromParams(params);
+  if (!post || post.draft) {
+    notFound();
+  }
+
+  return (
+    <article>
+      <div className="print:rounded-none rounded-xl print:border-none border print:bg-transparent bg-card text-card-foreground print:shadow-none shadow w-full">
+        <div className="flex flex-col space-y-1.5 p-6 pb-4">
+          <h1 className="text-2xl font-medium tracking-tight" id="post-title">
+            {post.title}
+          </h1>
+          <p className="text-muted-foreground text-sm flex flex-row gap-2">
+            <time>{formatDate(post.date)}</time>
+            <span> · </span>
+            <a className="hover:underline" href={`/category/${post.category}`}>
+              {post.category}
+            </a>
+          </p>
+        </div>
+
+        <div
+          data-orientation="horizontal"
+          role="none"
+          className="shrink-0 bg-border h-[1px] w-full"
+        ></div>
+        <div className="p-6 pt-4">
+          <div className="text-pretty hyphens-auto ">
+            <MDXContent code={post.body} />
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default PostPage;
--- velite.config.ts
+++ velite.config.ts
@@ -1,11 +1,19 @@
 import { defineCollection, defineConfig, s } from "velite";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // `s` is extended from Zod with some custom schemas,
 // you can also import re-exported `z` from `velite` if you don't need these extension schemas.
 
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
+
 const posts = defineCollection({
   name: "Post", // collection type name
-  pattern: "posts/**/*.md", // content files glob pattern
+  pattern: "post/**/*.mdx", // content files glob pattern
   schema: s
     .object({
       title: s.string().max(99), // Zod primitive type
@@ -14,13 +22,12 @@ const posts = defineCollection({
       category: s.string().max(20),
       cover: s.string().optional(),
       description: s.string().max(999).optional(),
-      body: s.markdown(),
+      body: s.mdx(),
       metadata: s.metadata(), // extract markdown reading-time, word-count, etc.
       draft: s.boolean().default(false),
       tags: s.array(s.string()).optional(),
     })
-    // more additional fields (computed fields)
-    .transform((data) => ({ ...data, permalink: `/blog/${data.slug}` })),
+    .transform(computedFields),
 });
 
 export default defineConfig({
@@ -33,4 +40,21 @@ export default defineConfig({
     clean: true,
   },
   collections: { posts },
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypePrettyCode, { theme: "github-dark" }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+    remarkPlugins: [],
+  },
 });

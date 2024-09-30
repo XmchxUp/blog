8e66dcd92d22a6ece2686a6a972b0b6f1dd85dbd
ultraman <1394466835@qq.com>
2024-09-30 09:24:45 +0800

    feat: optimize comment compoent

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, memo, useRef, useState } from "react";
 import {
   addComment,
   getComment,
-  GetCommentResponse,
   WalineRootComment,
   WalineComment,
+  WalineChildComment,
 } from "@waline/api";
 import Link from "next/link";
 import Image from "next/image";
@@ -14,28 +14,37 @@ import { Icons } from "./icons";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Label } from "./ui/label";
+import { useToast } from "@/hooks/use-toast";
+import { isValidEmail } from "@/lib/utils";
+import { Toaster } from "./ui/toaster";
 
 interface WalineCommentProps {
   postPath: string;
 }
 
+// TODO: add page
 const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
   const WALINE_SERVER_URL =
     "https://waline-git-main-xmchxups-projects.vercel.app/";
-  const [comments, setComments] = useState<GetCommentResponse>();
-  const formRef = useRef<HTMLDivElement>(null);
 
-  const [content, setContent] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [link, setLink] = useState<string>("");
-  const [title, setTitle] = useState<string>("Send comment");
-  const [rootID, setRootID] = useState<string>("");
-  const [replyID, setReplyID] = useState<string>("");
-  const [replyUser, setReplyUser] = useState<string>("");
+  const [commentTotal, setCommentTotal] = useState<number>(0);
+  const [comments, setComments] = useState<Array<WalineComment>>([]);
+  const formRef = useRef<HTMLDivElement>(null);
+  const { toast } = useToast();
+
+  const [formData, setFormData] = useState({
+    comment: "",
+    nick: "",
+    email: "",
+    link: "",
+    rid: "",
+    pid: "",
+    at: "",
+    title: "Send comment",
+  });
 
   useEffect(() => {
-    const fetchComment = async () => {
+    const fetchComments = async () => {
       try {
         const resp = await getComment({
           serverURL: WALINE_SERVER_URL,
@@ -45,63 +54,102 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
           sortBy: "insertedAt_desc",
           lang: navigator.language,
         });
-        setComments(resp);
+        setComments(resp.data);
+        setCommentTotal(resp.count);
       } catch (error) {
-        console.error("Failed to fetch comment count:", error);
+        console.error("Failed to fetch comments:", error);
+        toast({
+          title: "Fetch Comments Failed",
+          description: `${error}`,
+        });
       }
     };
-
-    fetchComment();
+    fetchComments();
   }, [postPath]);
 
-  const handleReset = () => {
-    setContent("");
-    setName("");
-    setEmail("");
-    setLink("");
-    setRootID("");
-    setReplyID("");
-    setReplyUser("");
+  const resetForm = () => {
+    setFormData({
+      comment: "",
+      nick: "",
+      email: "",
+      link: "",
+      rid: "",
+      pid: "",
+      at: "",
+      title: "Send comment",
+    });
   };
 
-  const handleSubmitComment = async () => {
+  const handleSubmitComment = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!formData.comment || !formData.email || !formData.nick) {
+      return;
+    }
+
+    if (!isValidEmail(formData.email)) {
+      toast({
+        title: "Invalid Email",
+        description: "Please enter a valid email address.",
+      });
+      return;
+    }
+
     const resp = await addComment({
       serverURL: WALINE_SERVER_URL,
       lang: navigator.language,
       comment: {
-        link,
-        mail: email,
-        nick: name,
-        comment: content,
+        ...formData,
         ua: navigator.userAgent,
         url: postPath,
         recaptchaV3: "6Ld1hFIqAAAAAMgMDfg8r7eEF6vFi5e548R6DlyS",
-        rid: rootID ?? null,
-        pid: replyID ?? null,
-        at: replyUser ?? null,
       },
     });
+
     console.log(resp);
+    if (resp.data) {
+      setComments((prevComments) => {
+        const updatedComments = [...prevComments];
+        const rootComment = updatedComments.find(
+          (c) => c.objectId === formData.rid
+        );
+
+        if (rootComment && isWalineRootComment(rootComment)) {
+          rootComment.children.push(resp.data as WalineChildComment);
+        } else if (resp.data) {
+          updatedComments.push(resp.data);
+        }
+        return updatedComments;
+      });
+      resetForm();
+    } else {
+      toast({
+        title: "Comment Failed",
+        description: `${resp.errmsg}`,
+      });
+    }
   };
 
   const handleReplyTo = (comment: WalineComment) => {
-    const findRootComment = (currentComment: WalineComment): WalineComment => {
-      if (!isWalineRootComment(currentComment)) {
-        const parentComment = comments?.data.find(
-          (c) => c.objectId === currentComment.pid
-        );
-        return parentComment ? findRootComment(parentComment) : currentComment;
-      }
-      return currentComment;
-    };
+    // console.log(isWalineRootComment(comment));
     const rootComment = findRootComment(comment);
-    console.log(rootComment.objectId, comment.nick, comment.objectId);
-    setRootID(rootComment.objectId);
-    setReplyUser(comment.nick);
-    setReplyID(comment.objectId);
-    setTitle(`Reply #${comment.objectId}`);
+    // console.log(rootComment.objectId, comment.nick, comment.objectId);
+    setFormData((prev) => ({
+      ...prev,
+      rid: rootComment.objectId,
+      at: comment.nick,
+      pid: comment.objectId,
+      title: `Reply #${comment.objectId}`,
+    }));
+    formRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
-    formRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  const findRootComment = (currentComment: WalineComment): WalineComment => {
+    return isWalineRootComment(currentComment)
+      ? currentComment
+      : findRootComment(
+          comments.find((c) => c.objectId === currentComment.pid) ||
+            currentComment
+        );
   };
 
   const isWalineRootComment = (
@@ -133,7 +181,7 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
                 <Image
                   className="aspect-square h-full w-full"
                   src={comment.avatar}
-                  alt={comment.nick}
+                  alt={comment.nick ?? "nickname"}
                   width={40}
                   height={40}
                 />
@@ -148,7 +196,9 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
           <div className="flex flex-col gap-1 flex-grow">
             <div className="flex flex-row gap-2 items-center justify-between w-full">
               <div className="inline-flex flex-row gap-2 items-center">
-                <div className="text-sm font-bold">{comment.nick}</div>
+                <div className="text-sm font-bold">
+                  {comment.nick ?? "unknow"}
+                </div>
                 <div className="text-sm text-secondary-foreground">
                   {new Date(comment.time).toLocaleString()}
                 </div>
@@ -184,7 +234,8 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
           </div>
         </div>
         {/* Only render 2 level comment */}
-        {isWalineRootComment(comment) &&
+        {!isChild &&
+          isWalineRootComment(comment) &&
           comment.children.map(
             (child) =>
               child.pid == comment.objectId && renderComment(child, true)
@@ -198,12 +249,12 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
       <div className="print:rounded-none rounded-xl print:border-none border print:bg-transparent bg-card text-card-foreground print:shadow-none shadow print:hidden">
         <div className="flex flex-col space-y-1.5 p-6">
           <h3 className="font-semibold leading-none tracking-tight">
-            Comments ({comments?.count})
+            Comments ({commentTotal})
           </h3>
         </div>
 
         <div className="p-6 pt-0 flex flex-col gap-4">
-          {comments?.data.map((comment) => renderComment(comment))}
+          {comments.map((comment) => renderComment(comment))}
         </div>
         <div
           data-orientation="horizontal"
@@ -212,13 +263,17 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
         ></div>
       </div>
 
+      <Toaster />
+
       {/* SendComment */}
       <div
         ref={formRef}
         className="print:rounded-none rounded-xl print:border-none border print:bg-transparent bg-card text-card-foreground print:shadow-none shadow print:hidden"
       >
         <div className="flex flex-col space-y-1.5 p-6">
-          <h3 className="font-semibold leading-none tracking-tight">{title}</h3>
+          <h3 className="font-semibold leading-none tracking-tight">
+            {formData.title}
+          </h3>
         </div>
         <div className="p-6 pt-0">
           <form className="grid grid-cols-2 gap-4">
@@ -227,8 +282,10 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
                 className="flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 resize-y"
                 placeholder="Comments"
                 name="comment"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
+                value={formData.comment}
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, comment: e.target.value }))
+                }
               />
               <p className="text-[0.8rem] text-muted-foreground">
                 Markdown supported. Please keep comments clean.
@@ -245,8 +302,10 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
                 className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
                 id="form-name-item"
                 name="nick"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={formData.nick}
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, nick: e.target.value }))
+                }
                 required
               />
             </div>
@@ -262,8 +321,11 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
                 required
                 id="form-email-item"
                 name="mail"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, email: e.target.value }))
+                }
               />
             </div>
             <div className="space-y-2 col-span-2">
@@ -277,8 +339,10 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
                 className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
                 id="form-link-item"
                 name="link"
-                value={link}
-                onChange={(e) => setLink(e.target.value)}
+                value={formData.link}
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, link: e.target.value }))
+                }
               />
             </div>
           </form>
@@ -288,7 +352,7 @@ const CustomWalineComment = ({ postPath }: WalineCommentProps) => {
             variant="ghost"
             className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2"
             type="reset"
-            onClick={handleReset}
+            onClick={resetForm}
           >
             Reset
           </Button>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -94,3 +94,8 @@ export function categorizedPostsByYear(posts: Array<Post>) {
 
   return res;
 }
+
+export const isValidEmail = (email: string) => {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+};

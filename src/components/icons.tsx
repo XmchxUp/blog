f9aca61e9d9ccdefabda03848c36e72d974e5411
xmchx <1394466835@qq.com>
2026-04-03 14:11:35 +0800

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,6 +1,25 @@
 type IconProps = React.HTMLAttributes<SVGElement>;
 
 export const Icons = {
+  dumbbell: (props: IconProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M14.5 6.5L17 4L20 7L17.5 9.5" />
+      <path d="M9.5 17.5L7 20L4 17L6.5 14.5" />
+      <path d="M6.5 9.5L4 7L7 4L9.5 6.5" />
+      <path d="M17.5 14.5L20 17L17 20L14.5 17.5" />
+      <path d="M12 12L12 12" />
+      <line x1="7" y1="12" x2="17" y2="12" />
+    </svg>
+  ),
   twitter: (props: IconProps) => (
     <svg
       {...props}
